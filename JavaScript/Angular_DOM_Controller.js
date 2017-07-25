84f641517d6d3449b2d48729f0f8d6dc2bf68cc8
AhmedShalaby11 <ahmedismaiell@gmail.com>
2017-07-25 23:03:11 +0200

    Adding Angular config
    1.Adding list items in main navigation bar with AngularJS as a configuration
    2.set the responsive style to list items ( mouserover , mouseout ,click ) effects with angular*
    
    *   <li ng-click="addClass='active'" ng-mouseleave="addClass=''" ng-mouseover="addClass='active'" class="{{addClass}}" ng-repeat="li in pagesURL"><a ng-href="{{li.url}}">{{li.label}}</a></li>

---
+++ JavaScript/Angular_DOM_Controller.js
@@ -0,0 +1,17 @@
+// Author : Ahmed Shalaby.
+// Info : AngularJS Class to control HTML Elements and it's configuartions.
+
+app.controller("DOM_listItem",function($scope){
+// list items configuration -- navigation bar
+$scope.pagesURL = [
+    {"label":"الرئيسيه","url":"index.html"},
+    {"label":"التسجيل","url":"Register.html"},
+    {"label":"اطلب حرفي","url":"searchResults.html"}
+    
+];
+// please make sure you add config here not hardcoded in html doc.
+// care of "," of the last array item.
+
+// 
+
+});
